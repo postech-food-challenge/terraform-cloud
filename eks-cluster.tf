2c5f413f6b37d329868be522b533e463f33aeb5b
dvdonadelli <davi_donadelli@live.com>
2024-04-11 22:18:15 -0300

    fix: update resource name on outputs and variables usage

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "food-challenge" {
   name     = "food-challenge"
-  role_arn = vars.awsAcademyRole 
+  role_arn = var.awsAcademyRole 
 
   vpc_config {
-    subnet_ids = ["${vars.subnetA}", "${vars.subnetB}", "${vars.subnetC}"]
-    security_group_ids = ["${vars.securityGroupId}"]
+    subnet_ids = ["${var.subnetA}", "${var.subnetB}", "${var.subnetC}"]
+    security_group_ids = ["${var.securityGroupId}"]
   }
 
   access_config {
