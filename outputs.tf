fc5576bbc966c31e304239769824f159e6df6483
dvdonadelli <davi_donadelli@live.com>
2024-04-13 14:37:03 -0300

    feat: creating our private image registry

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.tech_challenge_cluster.certificate_authority[0].data
 }
+
+output "ecr-repository-url" {
+  value = aws_ecr_repository.tech_challenge_image_registry.repository_url
+}
