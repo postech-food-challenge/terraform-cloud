ee64410d97f7d7e3a5d79890acbe6dbc393add6f
dvdonadelli <davi_donadelli@live.com>
2024-04-13 15:22:04 -0300

    fix: removing optional parameter group name

--- rds.tf
+++ rds.tf
@@ -6,7 +6,6 @@ resource "aws_db_instance" "food_challenge" {
   identifier             = "rds"
   username               = "foodchallenge"
   password               = "root1234"
-  parameter_group_name   = "default.mysql5.7"
   skip_final_snapshot    = true
   vpc_security_group_ids = [var.securityGroupId]
   tags = {
