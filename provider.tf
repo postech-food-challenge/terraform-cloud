953161a35330a012c5222bb066faa2010b272b02
dvdonadelli <davi_donadelli@live.com>
2024-04-13 15:48:19 -0300

    fix: trying to assume principal arn role

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn     = var.awsAcademyRole
+    role_arn     = var.principalArn
     session_name = "AWS_ACADEMY_SESSION"
   }
 }
