294e4967ee53695d7c07819875ea711aaac2ae21
dvdonadelli <davi_donadelli@live.com>
2024-04-15 21:25:17 -0300

    fix: trying again to provision rds - and full infrastructure because I've destroyed everything before

--- rds.tf
+++ rds.tf
@@ -1,11 +1,12 @@
 resource "aws_db_instance" "food_challenge" {
-  allocated_storage      = 10
+  allocated_storage      = 20
   db_name                = "foodchallenge"
   engine                 = "postgresql"
   instance_class         = "db.t3.micro"
   identifier             = "rds"
   username               = "foodchallenge"
   password               = "root1234"
+  multi_az               = false
   skip_final_snapshot    = true
   vpc_security_group_ids = [var.securityGroupId]
   tags = {
