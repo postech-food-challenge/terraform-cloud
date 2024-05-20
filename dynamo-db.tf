12e69b714cf03f2ae20d104125175b4e9b3bac9f
dvdonadelli <davi_donadelli@live.com>
2024-05-20 17:22:46 -0300

    fix: adding read and write capacities

--- dynamo-db.tf
+++ dynamo-db.tf
@@ -1,6 +1,8 @@
 resource "aws_dynamodb_table" "orders" {
-  name     = "orders"
-  hash_key = "id"
+  name           = "orders"
+  hash_key       = "id"
+  read_capacity  = 20
+  write_capacity = 20
 
   attribute {
     name = "id"
